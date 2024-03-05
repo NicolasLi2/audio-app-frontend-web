f8db74b367f73096e6fb839fa50057f6c5c5fc42
nicolas <linze201@gmail.com>
2024-03-05 22:31:47 +0800

    create basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Verification from './pages/Verification';
+import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
-import LostPassword from './pages/LostPassword';
-import Verification from './pages/Verification';
 
-function App() {
+const Layout = () => {
   return (
     <>
-      {/* <SignUp /> */}
-      {/* <SignIn /> */}
-      {/* <LostPassword /> */}
-      <Verification />
+      {/* <Header /> */}
+      <Outlet />
+      {/* <Footer /> */}
     </>
   );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/signin',
+        element: <SignIn />,
+      },
+      {
+        path: '/verify',
+        element: <Verification />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;

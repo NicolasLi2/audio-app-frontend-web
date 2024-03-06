0a38d86b8eb87e67940a444c172035510eb54844
nicolas <linze201@gmail.com>
2024-03-06 10:47:33 +0800

    create commonly used global CSS style; create Home page Layout template

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,33 @@ import Verification from './pages/Verification';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Header from './ui/Header';
+// import { Footer } from 'antd/es/layout/layout';
+import Footer from './ui/Footer';
 
-const Layout = () => {
-  return (
-    <>
-      {/* <Header /> */}
-      <Outlet />
-      {/* <Footer /> */}
-    </>
-  );
-};
+// const Layout = () => {
+//   return (
+//     <>
+//       <Header />
+//       <Outlet />
+//       <Footer />
+//     </>
+//   );
+// };
 
 const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
   {
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/signup',
-        element: <SignUp />,
-      },
-      {
-        path: '/signin',
-        element: <SignIn />,
-      },
-      {
-        path: '/verify',
-        element: <Verification />,
-      },
-    ],
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+  {
+    path: '/verify',
+    element: <Verification />,
   },
 ]);
 
---
+++ src/ui/Footer.tsx
@@ -0,0 +1,5 @@
+interface Props {}
+
+export default function Footer() {
+  return <div>Footer</div>;
+}
